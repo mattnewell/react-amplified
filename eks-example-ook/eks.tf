ed2fb3274941346843bb43c13ed1ee676356e27d
Matt Newell <matt.newell@ironnetcybersecurity.com>
2022-02-15 12:33:04 -0500

    Working eks-example-ook, using local branch of that repo rm-vpc-fallback.

--- eks-example-ook/eks.tf
+++ eks-example-ook/eks.tf
@@ -1,4 +1,16 @@
+variable "region" {
+  default     = "us-east-1"
+  description = "AWS region"
+}
+
+provider "aws" {
+  region = var.region
+}
+
 module "eks" {
-  source  = "Young-ook/eks/aws"
-  name    = "ook-eks"
+  name = "mn-ook-test"
+  source  = "../../terraform-aws-eks"
+#  version = "1.7.0"
+  kubernetes_version = "1.21"
+  subnets = ["subnet-02ac0a17015778d0f", "subnet-0c063bc4f1c897d0a", "subnet-0c842dea988ae67bd"]
 }
